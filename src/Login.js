abe6b0dc7a1e72a53da4d21e22e38241f2998685
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2024-11-06 23:02:25 -0300

    just fixing variables to auth

--- src/Login.js
+++ src/Login.js
@@ -5,14 +5,14 @@ import Cookies from 'js-cookie';
 import { API_BASE_URL } from './config';
 
 const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState('');
+  const [emailOrName, setEmailOrName] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
+      const response = await axios.post(`${API_BASE_URL}/login`, { emailOrName, password });
       const { token, userId } = response.data;
       
       Cookies.set('token', token);
@@ -28,7 +28,7 @@ const Login = ({ onLogin }) => {
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+      <input type="text" value={emailOrName} onChange={(e) => setEmailOrName(e.target.value)} placeholder="Email/Name" required />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
       <button type="submit">Login</button>
     </form>
