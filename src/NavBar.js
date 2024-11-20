43c8fd5d8ff4b799cd0579503e5b6746f97ec765
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2024-11-20 13:59:01 -0300

    reverting details in navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
+import './NavBar.css';
 
 const NavBar = () => {
   const { isAuthenticated, username, logout } = useAuth();
@@ -13,28 +14,28 @@ const NavBar = () => {
   };
 
   return (
-    <Navbar bg="light" expand="lg" fixed="top" className="shadow px-3">
+    <Navbar bg="light" expand="lg" fixed="top" className="shadow px-3 navbar-custom d-flex justify-content-between">
       <Navbar.Brand as={Link} to="/" className="fw-bold">
         My Shopping List
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ms-auto">
-        {isAuthenticated ? (
-          <>
-            <Nav.Link as={Link} to="#" className="fw-bold">
-             {username}
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="align-items-center">
+          {isAuthenticated ? (
+            <>
+              <span className="navbar-text fw-bold me-3">
+                {username}
+              </span>
+              <Nav.Link onClick={handleLogout} className="fw-bold">
+                Logout
+              </Nav.Link>
+            </>
+          ) : (
+            <Nav.Link as={Link} to="/login" className="fw-bold">
+              Login
             </Nav.Link>
-            <Nav.Link onClick={handleLogout} className="fw-bold">
-              Logout
-            </Nav.Link>
-          </>
-        ) : (
-          <Nav.Link as={Link} to="/login" className="fw-bold">
-            Login
-          </Nav.Link>
-        )}
-      </Nav>
+          )}
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
