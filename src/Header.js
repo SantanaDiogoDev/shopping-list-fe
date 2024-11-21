279f01dfebc367580d4796ee3c3367b0b80d2547
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2024-11-20 23:09:36 -0300

    finishing header v1. responsible

--- src/Header.js
+++ src/Header.js
@@ -15,14 +15,16 @@ const Header = () => {
   return (
     <header>
       <Link className='title' to="/">My Shopping List</Link>
-      {isAuthenticated ? (
-        <>
-          <Link className='username'> {username} </Link>
-          <Link className="login-logout" onClick={handleLogout}> Logout </Link>
-        </>
-        ) : (
-          <Link className="login-logout"> Login </Link>
+      <div className="nav-items">
+        {isAuthenticated ? (
+          <>
+            <Link className='username'> {username} </Link>
+            <Link className="login-logout" onClick={handleLogout}> Logout </Link>
+          </>
+          ) : (
+            <Link className="login-logout"> Login </Link>
         )}
+      </div>
     </header>
   );
 };
